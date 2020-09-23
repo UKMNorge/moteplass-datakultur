1b9d8be3333a9f337612c8a8070373907234571b
martematiker <33703596+martematiker@users.noreply.github.com>
2020-09-23 20:54:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('STANDALONE_URL', 'https://datakultur' . (UKM_HOSTNAME == 'ukm.dev' ? '.u
 define('STANDALONE_AJAX', 'false');
 define('STANDALONE_SECTION', 'Møteplass datakultur');
 define('STANDALONE_TITLE', 'Møteplass datakultur');
-define('STANDALONE_DESCRIPTION', 'Vega scene, Oslo, lørdag 25.januar');
+define('STANDALONE_DESCRIPTION', 'En rapport om ungdommens behov for flere sosiale arenaer med gaming i fokus');
 define('STANDALONE_TWIG_PATH', dirname(__FILE__) . '/twig/');
 
 require_once('vendor/autoload.php');
